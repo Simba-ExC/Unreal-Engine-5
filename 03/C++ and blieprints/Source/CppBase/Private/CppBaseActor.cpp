9ceb41b7f888388e701ec5fcd85d1da8fc2ec5bf
Simba-ExC <117112169+Simba-ExC@users.noreply.github.com>
2023-12-12 17:27:13 +0300

    Add files via upload

---
+++ 03/C++ and blieprints/Source/CppBase/Private/CppBaseActor.cpp
@@ -0,0 +1,65 @@
+﻿// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "CppBaseActor.h"
+#include "Engine/Engine.h"
+
+int ACppBaseActor::count = 0;
+
+// Sets default values
+ACppBaseActor::ACppBaseActor()
+{
+ 	// Set this actor to call Tick() every frame.  You can turn this off to improve performance if you don't need it.
+	PrimaryActorTick.bCanEverTick = true;
+
+}
+
+// Called when the game starts or when spawned
+void ACppBaseActor::BeginPlay()
+{
+	Super::BeginPlay();
+
+	ShowInformation();
+	//ShowActorInformation();
+}
+
+// Called every frame
+void ACppBaseActor::Tick(float DeltaTime)
+{
+	Super::Tick(DeltaTime);
+
+}
+
+void ACppBaseActor::ShowInformation()
+{
+	// ” мен¤ русский текст не выводитс¤? (
+	UE_LOG(LogTemp, Display, TEXT("CppBase is here"));
+	UE_LOG(LogTemp, Warning, TEXT("CppBase класс ошибся"));
+	UE_LOG(LogTemp, Error, TEXT("CppBase ай-ай-ай!"));
+
+	/*
+		каждый раз когда ¤ что то мен¤ю в VS,
+		нужно сначала закрыть UE, а затем запустить
+		программы и она снова открое UE или есть способ
+		пересобрать программу без закрыти¤ UE?
+	*/
+	
+	UE_LOG(LogTemp, Display, TEXT("PlayerName: %s"), *PlayerName);
+	GEngine->AddOnScreenDebugMessage(-1, 10.0f, FColor::Black, PlayerName, true, FVector2D(2.0f, 2.0f));
+	UE_LOG(LogTemp, Display, TEXT("currentHealth: %f"), currentHealth);
+}
+
+/*
+	Ќе знаю насколько правильно пон¤л задание,
+	что касаетс¤ (Instance name),
+	но все вроде работает как задумывалось)
+*/
+void ACppBaseActor::ShowActorInformation()
+{
+	id = count;
+	++count;
+	UE_LOG(LogTemp, Display, TEXT("Instance name: %s%d"), *PlayerName, id);
+	UE_LOG(LogTemp, Display, TEXT("EnemyNum: %d"), enemyNum);
+	UE_LOG(LogTemp, Display, TEXT("IsAlive: %i"), isAlive);
+}
+
---
+++ 03/C++ and blieprints/Source/CppBase/Public/CppBaseActor.h
@@ -0,0 +1,43 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/Actor.h"
+#include "CppBaseActor.generated.h"
+
+UCLASS()
+class CPPBASE_API ACppBaseActor : public AActor
+{
+	GENERATED_BODY()
+	
+public:	
+	// Sets default values for this actor's properties
+	ACppBaseActor();
+
+protected:
+	// Called when the game starts or when spawned
+	virtual void BeginPlay() override;
+
+	UPROPERTY(VisibleAnywhere)
+	FString PlayerName = "A_CppBase_C_";
+
+	UPROPERTY(EditAnywhere)
+	int enemyNum = 20;
+
+	UPROPERTY(EditDefaultsOnly)
+	float currentHealth = 57.54687;
+
+	UPROPERTY(EditInstanceOnly)
+	bool isAlive = true;
+
+public:	
+	// Called every frame
+	virtual void Tick(float DeltaTime) override;
+
+private:
+	void ShowInformation();
+	void ShowActorInformation();
+	static int count;
+	int id;
+};
